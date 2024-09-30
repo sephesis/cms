d2a8159a366df11f2d570647e1a3c2fdfd9fa320
sephesis <sephesis@yandex.ru>
2024-09-30 09:49:32 +0300

    react init

--- index.php
+++ index.php
@@ -1,10 +1,10 @@
 <?php
 
-$isInstalled = true;
+$isInstalled = false;
 
 require __DIR__. '/vendor/autoload.php';
 require __DIR__. '/app/functions.php';
-
+require __DIR__ . '/app/scripts.php';
 
 if ($isInstalled && file_exists(__DIR__. '/app.php')) {
     require __DIR__ . '/app.php';
